e944f57186601f8ad336e1ce51dd941262f06b93
“alivepages2” <ventas@livepages.com>
2021-06-03 08:14:04 -0500

    first commit

--- src/demo.js
+++ src/demo.js
@@ -1,76 +1,121 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
-import ImagePicker from './react-image-picker'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import ImagePicker from "./react-image-picker";
 
-import img1 from './assets/images/kitten/200.jpg'
-import img2 from './assets/images/kitten/201.jpg'
-import img3 from './assets/images/kitten/202.jpg'
-import img4 from './assets/images/kitten/203.jpg'
+import img1 from "./assets/images/cars/versa.jpeg";
+import img2 from "./assets/images/cars/crv.jpeg";
+import img3 from "./assets/images/cars/civic.jpeg";
+import img4 from "./assets/images/cars/tiguan.jpg";
+import img5 from "./assets/images/cars/sentra.jpeg";
+import img6 from "./assets/images/cars/vento.jpeg";
+import img7 from "./assets/images/cars/aveo.jpeg";
+import img8 from "./assets/images/cars/spark.jpeg";
 
-const imageList = [img1, img2, img3, img4]
+const data = [
+  {
+    description: " change of suspension",
+    make: "Nissan",
+    model: "Versa",
+    estimatedate: "2021/12/01",
+    id: 3340,
+    image: "http://3.23.108.188/cars/versa.jpg",
+  },
+  {
+    description: " motor adjustment",
+    make: " Honda ",
+    model: "CR-V",
+    estimatedate: "",
+    id: 3341,
+    image: "http://3.23.108.188/cars/CR-V.jpg",
+  },
+  {
+    description: " engine tuning ",
+    make: "Honda",
+    model: "Civic",
+    estimatedate: "2020/20/12",
+    id: 3342,
+    km: 90000,
+    image: "http://3.23.108.188/cars/civic.jpg",
+  },
+  {
+    description: " oil change ",
+    make: " Volkswaguen",
+    model: "Tiguan",
+    km: 13500,
+    id: 3343,
+    image: "http://3.23.108.188/cars/tiguan.jpg",
+  },
+  {
+    description: " change of pads ",
+    make: " Nissan ",
+    model: "Sentra",
+    km: 6000,
+    id: 3344,
+    image: "http://3.23.108.188/cars/sentra.jpg",
+  },
+  {
+    description: " change of pads ",
+    make: "Volkswagen",
+    model: "Vento",
+    km: 80050,
+    id: 3345,
+    image: "http://3.23.108.188/cars/vento.jpg",
+  },
+  {
+    description: "Change Transmission (CVT)",
+    make: "Chevrolet",
+    model: "Aveo NG",
+    estimatedate: "2021/09/07",
+    km: 33460,
+    image: "http://3.23.108.188/cars/aveo.jpg",
+  },
+  {
+    description: "Change ligths",
+    make: "Chevrolet",
+    model: "Spark",
+    km: 16098,
+    id: 3347,
+    image: "http://3.23.108.188/cars/spark.jpg",
+  },
+];
+
+const imageList = [img1, img2, img3, img4, img5, img6, img7, img8];
 
 class Demo extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      image: '',
       images: [],
-      max_images: [],
-      max_message: ''
-    }
+    };
+    this.onPick = this.onPick.bind(this);
   }
 
-  onPickImage(image) {
-    this.setState({image})
-  }
-
-  onPickImages(images) {
-    this.setState({images})
-  }
-
-  onPickImagesWithLimit(max_images) {
-    this.setState({max_images})
-  }
-
-  onPickMaxImages(last_image) {
-    let image = JSON.stringify(last_image)
-    let max_message = `Max images reached. ${image}`
-
-    this.setState({max_message})
+  onPick(images) {
+    this.setState({ images });
   }
 
   render() {
+    let mivar = null;
+    if (this.state.images) mivar = JSON.stringify(this.state.images);
     return (
       <div>
-        <h1>React Image Picker</h1>
-        <h3>Single Select</h3>
         <ImagePicker
-          images={imageList.map((image, i) => ({src: image, value: i}))}
-          onPick={this.onPickImage.bind(this)}
-        />
-        <textarea rows="4" cols="100" value={this.state.image && JSON.stringify(this.state.image)} disabled/>
-        
-        <h3>Multiple Select</h3>
-        <ImagePicker
-          images={imageList.map((image, i) => ({src: image, value: i}))}
-          onPick={this.onPickImages.bind(this)}
+          images={imageList.map((image, i) => ({ src: image, value: i }))}
+          onPick={this.onPick}
           multiple
         />
-        <textarea rows="4" cols="100" value={this.state.images && JSON.stringify(this.state.images)} disabled/>
-
-        <h3>Multiple Select with Limit</h3>
-        <ImagePicker
-          images={imageList.map((image, i) => ({src: image, value: i}))}
-          onPick={this.onPickImagesWithLimit.bind(this)}
-          maxPicks={2}
-          onMaxPicks={this.onPickMaxImages.bind(this)}
-          multiple
-        />
-        <textarea rows="4" cols="100" value={this.state.max_images && JSON.stringify(this.state.max_images)} disabled/>
-        <textarea rows="4" cols="100" value={this.state.max_message && JSON.stringify(this.state.max_message)} disabled/>
+        <button
+          type="button"
+          onClick={() => {
+            this.mantenimiento = this.state.value;
+            console.log(mivar);
+          }}
+        >
+          Mantenimento
+        </button>
       </div>
-    )
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#root'))
+render(<Demo />, document.querySelector("#root"));
